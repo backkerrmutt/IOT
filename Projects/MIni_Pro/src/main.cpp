6a00b5f7a42089dbcc7684b0433311fc54944dc2
backker <backker@mail.com>
2024-09-30 23:24:29 +0700

    add main.c

---
+++ Projects/MIni_Pro/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hi esp");
+}
+
