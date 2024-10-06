ed49965de6824bdf40feadd6a69358f3fbeaeeb4
backker <backker@mail.com>
2024-10-06 18:15:23 +0700

    version 1.0

--- Projects/test_sensor_max30102/src/main.cpp
+++ Projects/test_sensor_max30102/src/main.cpp
@@ -1,20 +1,68 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-#include <MAX30100_PulseOximeter.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+#include "LiquidCrystal_I2C.h"
+MAX30105 particleSensor;
+LiquidCrystal_I2C lcd(0x57,16,2);
+const int numReadings = 10; // Number of readings for the moving average
+float readings[numReadings]; // Array to store the readings
+int readIndex = 0; // Index of the current reading
+float total = 0; // Total of the readings
+float average = 0; // Average of the readings
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  particleSensor.begin(Wire, I2C_SPEED_FAST);
+  particleSensor.setup();
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Initialize all readings to 0
+  for (int i = 0; i < numReadings; i++) {
+    readings[i] = 0;
+  }
+}
+
+long getTimeBetweenBeats() {
+  static long lastBeat = 0;
+  long currentBeat = millis();
+  long timeBetweenBeats = currentBeat - lastBeat;
+  lastBeat = currentBeat;
+  return timeBetweenBeats;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  long irValue = particleSensor.getIR();
+  if (checkForBeat(irValue) == true) {
+    float beatsPerMinute = 60 / (getTimeBetweenBeats() / 1000.0);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Subtract the last reading
+    total = total - readings[readIndex];
+    // Add the new reading
+    readings[readIndex] = beatsPerMinute;
+    total = total + readings[readIndex];
+    // Advance to the next position in the array
+    readIndex = readIndex + 1;
+
+    // If we're at the end of the array, wrap around to the beginning
+    if (readIndex >= numReadings) {
+      readIndex = 0;
+    }
+
+    // Calculate the average
+    average = total / numReadings;
+
+    Serial.print("BPM: ");
+    Serial.println(average);
+    
+    lcd.setCursor(0,0);
+    lcd.print(average);
+    
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
